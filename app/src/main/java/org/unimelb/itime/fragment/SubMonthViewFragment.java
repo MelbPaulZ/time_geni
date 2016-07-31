33c997e3e1285542aa1c4b98a93eb418ab1cb3bc
yinchuandong <yincd520@sina.com>
2016-07-31 23:21:32 +1000

    check

---
+++ app/src/main/java/org/unimelb/itime/fragment/SubMonthViewFragment.java
@@ -0,0 +1,12 @@
+package org.unimelb.itime.fragment;
+
+import org.unimelb.itime.R;
+import org.unimelb.itime.base.BaseUiAuthFragment;
+
+public class SubMonthViewFragment extends BaseUiAuthFragment{
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_sub_month_view;
+    }
+}
