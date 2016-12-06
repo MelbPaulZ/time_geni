3048e84455ce5248f9ab5ff4db7e591b7ccc295e
MelbPaulZ <Paul@Pauls-MacBook-Pro.local>
2016-12-07 00:03:02 +1100

    todo: fix solo event unitl

--- app/src/main/java/org/unimelb/itime/ui/presenter/CommonPresenter.java
+++ app/src/main/java/org/unimelb/itime/ui/presenter/CommonPresenter.java
@@ -7,7 +7,9 @@ import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
 import com.hannesdorfmann.mosby.mvp.MvpView;
 
 import org.greenrobot.eventbus.EventBus;
+import org.unimelb.itime.R;
 import org.unimelb.itime.bean.Event;
+import org.unimelb.itime.managers.DBManager;
 import org.unimelb.itime.managers.EventManager;
 import org.unimelb.itime.messageevent.MessageEvent;
 import org.unimelb.itime.restfulapi.EventApi;
@@ -17,6 +19,8 @@ import org.unimelb.itime.util.HttpUtil;
 
 import rx.Observable;
 import rx.Subscriber;
+
+import static java.security.AccessController.getContext;
 
 /**
  * Created by Paul on 3/10/16.
@@ -63,6 +67,61 @@ public class CommonPresenter extends MvpBasePresenter<MvpView> {
         EventManager.getInstance().updateEvent(oldEvent, newEvent);
         EventManager.getInstance().getWaitingEditEventList().remove(oldEvent);
         EventBus.getDefault().post(new MessageEvent(MessageEvent.RELOAD_EVENT));
+    }
+
+    public void updateOnlyThisEvent(Event orgEvent, Event newEvent){
+        updateOrgEventToServer(orgEvent);
+        insertNewEventToServer(newEvent);
+    }
+
+    private void updateOrgEventToServer(Event orgEvent){
+        Observable<HttpResult<Event>> observable = eventApi.update(CalendarUtil.getInstance().getCalendar().get(0).getCalendarUid(), orgEvent.getEventUid(), orgEvent);
+        Subscriber<HttpResult<Event>> subscriber = new Subscriber<HttpResult<Event>>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onNext(HttpResult<Event> eventHttpResult) {
+                synchronizeLocal(eventHttpResult.getData());
+                EventBus.getDefault().post(new MessageEvent(MessageEvent.RELOAD_EVENT));
+            }
+        };
+        HttpUtil.subscribe( observable ,  subscriber);
+    }
+
+    private void insertNewEventToServer(Event event){
+        Observable<HttpResult<Event>> observable = eventApi.insert(event);
+        Subscriber<HttpResult<Event>> subscriber = new Subscriber<HttpResult<Event>>() {
+            @Override
+            public void onCompleted() {
+                Log.i(TAG, "onCompleted: ");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onNext(HttpResult<Event> eventHttpResult) {
+                Event ev = eventHttpResult.getData();
+                insertEventLocal(ev);
+                EventBus.getDefault().post(new MessageEvent(MessageEvent.RELOAD_EVENT));
+            }
+        };
+        HttpUtil.subscribe(observable,subscriber);
+    }
+
+    private void insertEventLocal(Event event){
+        EventManager.getInstance().addEvent(event);
+        DBManager.getInstance().insertEvent(event);
     }
 
 
