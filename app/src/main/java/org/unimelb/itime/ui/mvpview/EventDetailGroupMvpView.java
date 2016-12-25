c4b6f9e478be8520bb9f088da714b84753b724ec
MelbPaulZ <ppsunshing@gmail.com>
2016-12-25 23:58:52 +1100

    change: edit delete btn and detail btns

--- app/src/main/java/org/unimelb/itime/ui/mvpview/EventDetailGroupMvpView.java
+++ app/src/main/java/org/unimelb/itime/ui/mvpview/EventDetailGroupMvpView.java
@@ -6,7 +6,6 @@ import org.unimelb.itime.bean.Timeslot;
  * Created by Paul on 4/09/2016.
  */
 public interface EventDetailGroupMvpView extends EventCommonMvpView{
-    void toCalendar();
     void toEditEvent();
     void viewInCalendar();
     void viewInviteeResponse(Timeslot timeSlot);
