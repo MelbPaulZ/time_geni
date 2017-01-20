9a98cece664fc63db85f87f70b65dbab96fa8d9d
MelbPaulZ <ppsunshing@gmail.com>
2017-01-20 15:53:34 +1100

    fix: calendar preference stack

--- app/src/main/java/org/unimelb/itime/ui/fragment/calendars/CalendarAgendaFragment.java
+++ app/src/main/java/org/unimelb/itime/ui/fragment/calendars/CalendarAgendaFragment.java
@@ -19,8 +19,6 @@ import org.unimelb.itime.messageevent.MessageEvent;
 import org.unimelb.itime.messageevent.MessageEventRefresh;
 import org.unimelb.itime.messageevent.MessageMonthYear;
 import org.unimelb.itime.managers.EventManager;
-import org.unimelb.itime.ui.presenter.EventCommonPresenter;
-import org.unimelb.itime.util.EventUtil;
 import org.unimelb.itime.vendor.agendaview.AgendaViewBody;
 import org.unimelb.itime.vendor.agendaview.MonthAgendaView;
 import org.unimelb.itime.vendor.helper.MyCalendar;
--- app/src/main/java/org/unimelb/itime/ui/fragment/calendars/CalendarMonthDayFragment.java
+++ app/src/main/java/org/unimelb/itime/ui/fragment/calendars/CalendarMonthDayFragment.java
@@ -19,7 +19,6 @@ import org.unimelb.itime.messageevent.MessageEventRefresh;
 import org.unimelb.itime.messageevent.MessageMonthYear;
 import org.unimelb.itime.managers.EventManager;
 import org.unimelb.itime.ui.mvpview.EventCommonMvpView;
-import org.unimelb.itime.ui.presenter.EventCommonPresenter;
 import org.unimelb.itime.ui.viewmodel.ToolbarViewModel;
 import org.unimelb.itime.util.AppUtil;
 import org.unimelb.itime.vendor.dayview.MonthDayView;
@@ -35,7 +34,6 @@ import java.util.List;
 public class CalendarMonthDayFragment extends CalendarBaseViewFragment {
     private View root;
     private MonthDayView monthDayView;
-    private EventCommonPresenter presenter;
     private String TAG = "MonthDayFragment";
     private EventManager eventManager;
 
